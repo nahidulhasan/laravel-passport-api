21f7127c9c5f2df099c42d3f3f6dbb8602454752
Nahidul Hasan <nahidul.cse@gmail.com>
2018-10-07 13:58:30 +0600

    Integrate passport

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
